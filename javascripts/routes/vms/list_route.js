59593462d4027239250e2068c0c4f533b16019ec
erifeh <Eric.FEHR@publicis-modem.fr>
2015-07-06 15:31:50 +0200

    Add vms refresh list calls

--- javascripts/routes/vms/list_route.js
+++ javascripts/routes/vms/list_route.js
@@ -5,8 +5,26 @@ var VmsListRoute = AuthenticatedRoute.extend({
   // Init model for the list
   model: function() {
     // Get all vms from ember datas
-    return this.store.all('vm').filterBy('project').sort(['project']) ;
+    return this.store.find('vm').filterBy('project').sort(['project']) ;
   },
+  setupController: function(controller, model){
+    this._super(controller, model);
+    this.startRefreshing();
+  },
+  startRefreshing: function(){
+    this.set('refreshing', true);
+    Ember.run.later(this, this.refresh, 30000);
+  },
+  refresh: function(){
+    if(!this.get('refreshing')) return;
+    this.store.find('vm').filterBy('project').sort(['project']);
+    Em.run.later(this, this.refresh, 30000);
+  },
+  actions:{
+    willTransition: function(){
+      this.set('refreshing', false);
+    }
+  }
 });
 
 module.exports = VmsListRoute;
