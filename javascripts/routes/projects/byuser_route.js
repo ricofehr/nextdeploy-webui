bdcd2f7003ac44121b86069d1788e10d2ce067b7
ricofehr <ricofehr@nextdeploy.io>
2015-10-27 21:59:56 +0100

    fix issues about cross navigation

--- javascripts/routes/projects/byuser_route.js
+++ javascripts/routes/projects/byuser_route.js
@@ -4,17 +4,17 @@ var AuthenticatedRoute = require('../authenticated_route');
 var ProjectsByuserRoute = AuthenticatedRoute.extend({
   // Get the projects following an user_id
   model: function(params) {
-    return this.store.find('project', { user_id: params.user_id }).filterBy('name') ;
+    return this.store.find('project', { user_id: params.user_id });
   },
 
   // Same template than the standard list
   renderTemplate:function () {
-    this.render('projects/list') ;
+    this.render('projects/list');
   },
 
   // Setup the controller "projects.list" with this model
   setupController: function(controller, model) {
-    this.controllerFor('projects.list').setProperties({content:model});
+    this.controllerFor('projects.list').setProperties(model);
   },
 });
 
--- javascripts/routes/vms/byuser_route.js
+++ javascripts/routes/vms/byuser_route.js
@@ -4,7 +4,7 @@ var AuthenticatedRoute = require('../authenticated_route');
 var VmsByuserRoute = AuthenticatedRoute.extend({
   // Get the vms following an user_id
   model: function(params) {
-    return this.store.find('vm', { user_id: params.user_id }).filterBy('project') ;
+    return this.store.find('vm', { user_id: params.user_id });
   },
 
   // Same template than the standard list of vms
@@ -14,7 +14,7 @@ var VmsByuserRoute = AuthenticatedRoute.extend({
 
   // Setup the controller for vms.list with this model
   setupController: function(controller, model) {
-    this.controllerFor('vms.list').setProperties({content:model});
+    this.controllerFor('vms.list').setProperties(model);
   },
 });
 
