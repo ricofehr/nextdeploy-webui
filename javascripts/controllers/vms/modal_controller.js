b20294f7648050b3f6162bebed29d34f5c2ac8f1
erifeh <Eric.FEHR@publicis-modem.fr>
2015-11-20 17:01:12 +0100

    Manage https uris and add credentials to uris only for chrome browser

--- javascripts/controllers/vms/modal_controller.js
+++ javascripts/controllers/vms/modal_controller.js
@@ -1,5 +1,6 @@
 // Ember controller for vm modals
 var VmsModalController = Ember.ObjectController.extend({
+  isHTTPS: false,
   // show / hide modals
   showUri: function() {
     $('#modaluris').modal('show');
@@ -61,8 +62,16 @@ var VmsModalController = Ember.ObjectController.extend({
     return false;    
   }.property('project'),
 
+  // reset isHTTPS property
+  resetHTTPS: function () {
+    this.set('isHTTPS', false);
+  }.property('project'),
 
   actions: {
+    // toggle https property
+    toggleHTTPS: function() {
+      this.toggleProperty('isHTTPS') ;
+    },
     // open vm uri
     openVmURI: function(uritype) {
       var login = this.get('project.login');
@@ -73,65 +82,77 @@ var VmsModalController = Ember.ObjectController.extend({
       var uri_xmlhttp_req = '';
       var uri_status = 'http://' + uri + '/status_ok';
       var popup = null;
+      var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+      var scheme = 'http';
+
+      if (this.get('isHTTPS')) {
+        scheme = 'https';
+      }
       
 
       switch(uritype) {
         case 'main':
-          uri_with_creds = 'http://' + authcreds + uri + '/';
-          uri_xmlhttp_req = 'http://' + uri + '/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/';
           break;
         case 'admin':
-          uri_with_creds = 'http://' + authcreds + 'admin.' + uri + '/';
-          uri_xmlhttp_req = 'http://admin.' + uri +'/';
+          uri_with_creds = scheme + '://' + authcreds + 'admin.' + uri + '/';
+          uri_xmlhttp_req = scheme + '://admin.' + uri +'/';
           break;
         case 'mobile':
-          uri_with_creds = 'http://' + authcreds + 'm.' + uri +'/';
-          uri_xmlhttp_req = 'http://m.' + uri +'/';
+          uri_with_creds = scheme + '://' + authcreds + 'm.' + uri +'/';
+          uri_xmlhttp_req = scheme + '://m.' + uri +'/';
           break;
         case 'html':
-          uri_with_creds = 'http://' + authcreds + uri + '/_html/';
-          uri_xmlhttp_req = 'http://' + uri + '/_html/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/_html/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/_html/';
           break;
         case 'nodejs':
-          uri_with_creds = 'http://' + authcreds + 'nodejs.' + uri + '/';
-          uri_xmlhttp_req = 'http://nodejs.' + uri + '/';
+          uri_with_creds = scheme + '://' + authcreds + 'nodejs.' + uri + '/';
+          uri_xmlhttp_req = scheme + '://nodejs.' + uri + '/';
           break;
         case 'gitpull':
-          uri_with_creds = 'http://' + authcreds + uri + '/pm_tools/gitsync/';
-          uri_xmlhttp_req = 'http://' + uri + '/pm_tools/gitsync/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/pm_tools/gitsync/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/pm_tools/gitsync/';
           break;
         case 'phpmyadmin':
-          uri_with_creds = 'http://' + authcreds + uri + '/pm_tools/phpmyadmin/';
-          uri_xmlhttp_req = 'http://' + uri + '/pm_tools/phpmyadmin/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/pm_tools/phpmyadmin/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/pm_tools/phpmyadmin/';
           break;
         case 'tail':
-          uri_with_creds = 'http://' + authcreds + uri + '/pm_tools/tail/';
-          uri_xmlhttp_req = 'http://' + uri + '/pm_tools/tail/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/pm_tools/tail/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/pm_tools/tail/';
           break;
         case 'phpinfo':
-          uri_with_creds = 'http://' + authcreds + uri + '/pm_tools/pminfo/';
-          uri_xmlhttp_req = 'http://' + uri + '/pm_tools/pminfo/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/pm_tools/pminfo/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/pm_tools/pminfo/';
           break;
         case 'clearvarnish':
-          uri_with_creds = 'http://' + authcreds + uri + '/pm_tools/clearvarnish/';
-          uri_xmlhttp_req = 'http://' + uri + '/pm_tools/clearvarnish/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/pm_tools/clearvarnish/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/pm_tools/clearvarnish/';
           break;
         case 'composerinstall':
-          uri_with_creds = 'http://' + authcreds + uri + '/pm_tools/composerinstall/';
-          uri_xmlhttp_req = 'http://' + uri + '/pm_tools/composerinstall/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/pm_tools/composerinstall/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/pm_tools/composerinstall/';
           break;
         case 'sf2cmds':
-          uri_with_creds = 'http://' + authcreds + uri + '/pm_tools/sf2/';
-          uri_xmlhttp_req = 'http://' + uri + '/pm_tools/sf2/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/pm_tools/sf2/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/pm_tools/sf2/';
           break;
         case 'sf2logs':
-          uri_with_creds = 'http://' + authcreds + uri + '/pm_tools/tailsf2/';
-          uri_xmlhttp_req = 'http://' + uri + '/pm_tools/tailsf2/';
+          uri_with_creds = scheme + '://' + authcreds + uri + '/pm_tools/tailsf2/';
+          uri_xmlhttp_req = scheme + '://' + uri + '/pm_tools/tailsf2/';
           break;
       }
       
       if (uri_with_creds == '') {
         return;
+      }
+
+      if (is_chrome) {
+        window.open(uri_with_creds);
+      } else {
+        window.open(uri_xmlhttp_req);
       }
 
       //popup = window.open(uri_status);
@@ -152,8 +173,9 @@ var VmsModalController = Ember.ObjectController.extend({
           //window.open(uri_xmlhttp_req);
       //  }
       //} else {
-        window.open(uri_with_creds);
+      //  window.open(uri_with_creds);
       //}
+      //
     }
   }
 });
