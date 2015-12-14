d124e109f160fd502250ebf60a831c8155a17ee8
erifeh <Eric.FEHR@publicis-modem.fr>
2015-12-14 13:31:53 +0100

    Fix a bug on brand form

--- javascripts/routes/brands/new_route.js
+++ javascripts/routes/brands/new_route.js
@@ -5,15 +5,16 @@ var BrandsNewRoute = AuthenticatedRoute.extend({
   // Empty model
   model: function() {
     return Ember.RSVP.hash({
-      brands: this.store.all('brand').filterBy('name')
+      brands: this.store.all('brand')
     });
   },
 
   // Setup controller
   setupController: function(controller, model) {
     content = Ember.Object.create() ;
+    this.controllerFor('brands.new').setProperties({model: content}) ;
     this.controllerFor('brands.new').clearForm() ;
-    this.controllerFor('brands.new').setProperties({content:content, brands: model.brands});
+    this.controllerFor('brands.new').setProperties({model: content, brands: model.brands});
   },
 });
 
