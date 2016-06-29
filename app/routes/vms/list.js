277dacd1e9d66bff78890169cfdaaaffddfea1bd
ricofehr <ricofehr@nextdeploy.io>
2016-06-29 23:24:26 +0200

    Display prod vms on the top of the list

--- app/routes/vms/list.js
+++ app/routes/vms/list.js
@@ -5,8 +5,33 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
 
     return Ember.RSVP.hash({
+      // sort by status, prod flag and id
       vms: this.store.peekAll('vm').toArray().sort(function(a, b) {
-        return Ember.compare(parseInt(b.id), parseInt(a.id));
+        var prod_a = a.get('is_prod') ? 1 : 0;
+        var prod_b = b.get('is_prod') ? 1 : 0;
+        var status_a = parseInt(a.get('status'));
+        var status_b = parseInt(b.get('status'));
+
+        if (status_a !== 1) {
+          status_a = status_a > 1 ? 0 : (-status_a)
+        }
+    
+        if (status_b !== 1) {
+          status_b = status_b > 1 ? 0 : (-status_b)    
+        }
+        
+        var comp_status = Ember.compare(status_b, status_a);
+        var comp_prod = Ember.compare(prod_b, prod_a);
+
+        if (comp_status === 0 && comp_prod === 0) {
+          return Ember.compare(parseInt(b.get('id')), parseInt(a.get('id')));
+        }
+
+        if (comp_status === 0) {
+          return comp_prod;
+        }
+
+        return comp_status;
       })
     });
   },
