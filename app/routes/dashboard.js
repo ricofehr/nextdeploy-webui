bd1b25a26f774873ff64057b38ac4f93c6609e48
ricofehr <ricofehr@nextdeploy.io>
2017-07-06 00:25:02 +0200

    Avoid unload hpmessages promises

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -4,9 +4,8 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
-    this.store.unloadAll('hpmessage');
     return Ember.RSVP.hash({
-      posts: this.store.findAll('hpmessage', { backgroundReload: false, reload: true }),
+      posts: this.store.peekAll('hpmessage')
     });
   }
 });
